6bdb80a17eb7b01d2658649516036abbc76925c8
Isaac Rossi <isaacjohnrossi@gmail.com>
2021-12-14 21:59:15 +1100

    fixed up some padding issues with the hero image

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,18 +12,22 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<!-- hero needs bg image -->
-	<section class="hero min-vh-100 cover bg-right" style="
-		<?php if( get_field('hero_image') ): ?>
+	<section class="hero min-vh-100">
+
+		<div class="hero-slideshow cover bg-right"  style="
+			<?php if( get_field('hero_image') ): ?>
 				background-image: url(<?php the_field('hero_image'); ?>);
 			<?php endif; ?>
-	">
+		">
+		</div>
+
 		<div class="hero-menu pl5">
-			<h1 class="mv0 pv4"><img src="<?php echo get_template_directory_uri() . '/images/logo.svg'; ?>" />
+			<h1 class="pt4 mt0 mb2"><img src="<?php echo get_template_directory_uri() . '/images/logo.svg'; ?>" />
 			</h1>
 			<nav>
 				<ul class="ma0 pl0">
-					<li class="mb2"><img src="<?php echo get_template_directory_uri() . '/images/youtube.svg'; ?>" /></li>
-					<li class="mb2"><img src="<?php echo get_template_directory_uri() . '/images/instagram.svg'; ?>" /></li>
+					<li><img src="<?php echo get_template_directory_uri() . '/images/youtube.svg'; ?>" class="mb2"/></li>
+					<li><img src="<?php echo get_template_directory_uri() . '/images/instagram.svg'; ?>" class="mb2"/></li>
 					<li><img src="<?php echo get_template_directory_uri() . '/images/email.svg'; ?>" /></li>
 				</ul>
 			</nav>
@@ -32,10 +36,10 @@
 		<div class="hero-content pl5">
 			<!-- country -->
 			<?php if( get_field('country') ): ?>
-				<p class="gothic light tracked mv0"><?php the_field('country'); ?></p>
+				<p class="gothic light ttu tracked mv0"><?php the_field('country'); ?></p>
 			<?php endif; ?>
 			<!-- title -->
-			<h1 class="gothic light f-headline mv0"> <?php the_title(); ?></h1>
+			<h1 class="gothic light mv2  f-headline lh-solid"> <?php the_title(); ?></h1>
 			<!-- subhead -->
 			<?php if( get_field('subhead') ): ?>
 				<p class="subhead editorial light f5 mv0"><?php the_field('subhead'); ?></p>
@@ -43,10 +47,11 @@
 		</div>
 
 		<nav class="hero-changer flex space-between">
-				<span class="change-slide mr2 br-100 ba bw-1"></span>
-				<span class="change-slide mr2 br-100 ba bw-1"></span>
+				<span class="change-slide br-100 ba bw-1"></span>
+				<span class="change-slide br-100 ba bw-1"></span>
 				<span class="change-slide br-100 ba bw-1"></span>
 		</nav>
+
 	</section>
 
 	<header class="entry-header">
