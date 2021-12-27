8d985b792a31d248c287dc82ad8bad3db8913734
Isaac Rossi <isaacjohnrossi@gmail.com>
2021-12-27 18:40:34 +1100

    seperated the gallery into its own file

--- template-parts/content.php
+++ template-parts/content.php
@@ -21,24 +21,9 @@
 			if ( get_row_layout() == 'text_block' ): ?>
 				<?php get_template_part('template-parts/content-text'); ?>
 
-				<?php elseif ( get_row_layout() == 'gallery' ) : ?>
+			<?php elseif ( get_row_layout() == 'gallery' ) : ?>
 
-					<div class="gallery container flex flex-wrap center ph4-l">
-						<?php $images = get_sub_field('gallery'); ?>
-						<?php foreach($images as $image) : ?>
-							<!-- here we have added a custom field to our attachment (images) which adds a class name to control its width-->
-							<div class="gallery-image center ph3 mb4 <?php the_field('image_width', $image['id']) ?>">
-								<?php echo wp_get_attachment_image($image['id'], "full"); ?>
-								<!-- here we assign our caption to a variable and if its not empty we display it on the page -->
-								<?php $caption = wp_get_attachment_caption($image['id']); ?>
-								<?php if(!empty($caption)) : ?>
-									<p class="caption editorial f5 o-50 pt3 mv0">
-										<?php echo $caption; ?>
-									</p>
-								<?php endif; ?>	
-							</div>
-						<?php endforeach; ?>
-					</div>		
+				<?php get_template_part('template-parts/content-gallery'); ?>	
 
 			<?php endif;
 		endwhile; endif; ?>
