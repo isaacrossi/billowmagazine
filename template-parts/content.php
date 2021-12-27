aa7add0cb0324af46a74f58a94986ad40d97a4c2
Isaac Rossi <isaacjohnrossi@gmail.com>
2021-12-27 18:30:00 +1100

    added custom widths to our images using acf

--- template-parts/content.php
+++ template-parts/content.php
@@ -23,10 +23,11 @@
 
 				<?php elseif ( get_row_layout() == 'gallery' ) : ?>
 
-					<div class="container flex flex-wrap center ph4-l">
+					<div class="gallery container flex flex-wrap center ph4-l">
 						<?php $images = get_sub_field('gallery'); ?>
 						<?php foreach($images as $image) : ?>
-							<div class="gallery-image ph3 mb4">
+							<!-- here we have added a custom field to our attachment (images) which adds a class name to control its width-->
+							<div class="gallery-image center ph3 mb4 <?php the_field('image_width', $image['id']) ?>">
 								<?php echo wp_get_attachment_image($image['id'], "full"); ?>
 								<!-- here we assign our caption to a variable and if its not empty we display it on the page -->
 								<?php $caption = wp_get_attachment_caption($image['id']); ?>
