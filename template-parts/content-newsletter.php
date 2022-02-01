0853d499cf49f817743cb2e3695acca79f047aa0
Isaac Rossi <isaacjohnrossi@gmail.com>
2022-02-02 10:45:36 +1100

    finished styling the newsletter section

--- template-parts/content-newsletter.php
+++ template-parts/content-newsletter.php
@@ -4,9 +4,9 @@
     <p class="newsletter-body editorial light f4 ma0">Sign up for our monthly magazine and we’ll keep you inspired with all the best surf and travel experiences from around the globe.</p>
   </div>
   <div class="">
-    <form>
-      <input type="email" required="true" placeholder="Enter your email">
-      <input type="submit">
+    <form class="flex flex-column">
+      <input class="email mb1 editorial" type="email" required="true" placeholder="Enter your email">
+      <input class="button accent-bg light ttu gothic tracked" type="submit" value="keep me in the loop">
     </form>
   </div> 
 </section>
