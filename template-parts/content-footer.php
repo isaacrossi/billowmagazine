78250caed0668fe3c33488f7bc61c1ca92ea0ef5
Isaac Rossi <isaacjohnrossi@gmail.com>
2022-02-09 15:51:27 +1100

    added responsive styles to the footer for smaller screens

--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -1,18 +1,18 @@
 <footer class="accent-bg">
 
-  <div  class="center container flex flex-row justify-between ph6 pv4 ">
+  <div  class="center container flex flex-column flex-row-l tc tl-l justify-between ph6 pv4 ">
 
     <div>
 
       <img class="footer-logo mb3" src="<?php echo get_template_directory_uri() . '/images/logo.svg'; ?>">
 
-      <nav class="flex flex-row items-end">
+      <nav class="flex flex-row items-end justify-start-l justify-center">
         <a class="mr2"><img src="<?php echo get_template_directory_uri() . '/images/youtube.svg'; ?>"></a>
         <a class="mr2"><img src="<?php echo get_template_directory_uri() . '/images/instagram.svg'; ?>"></a>
         <a><img src="<?php echo get_template_directory_uri() . '/images/email.svg'; ?>"></a>
       </nav>
 
-      <p class="editorial f5 light ma0 mt3 lh-copy">Billow Magazine 2022 © <br> All Rights Reserved</p>
+      <p class="editorial f5 light ma0 mt3 lh-copy center-m mb4-m">Billow Magazine 2022 © <br> All Rights Reserved</p>
 
       </div>
 
@@ -21,12 +21,12 @@
       <p class="editorial light f5 ma0 mb2">Oceania</p>
       <p class="editorial light f5 ma0 mb2">Asia</p>
       <p class="editorial light f5 ma0 mb2">South America</p>
-      <p class="editorial light f5 ma0 mb2">North America</p>
+      <p class="editorial light f5 ma0 mb2 mb4-m">North America</p>
       </div>
 
       <div>
       <p class="gothic light f3 ma0 mb3">About</p>
-      <p class="about-text editorial light f5 ma0 mb2 lh-copy">We are a surf and travel magazine born out of a desire to get more people out there experiencing cool sh*t.</p>
+      <p class="about-text editorial light f5 ma0 mb2 lh-copy center-m">We are a surf and travel magazine born out of a desire to get more people out there experiencing cool sh*t.</p>
     </div>
 
   </div>
